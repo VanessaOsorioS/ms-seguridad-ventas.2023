d62a1058a8996d1b9c62a443b96828ca2d0e0ef9
vanessa <vanessa.1702011634@ucaldas.edu.co>
2023-03-19 17:18:42 -0500

    cuerpo de la autenticacion de un usuario frente a una accion en la base de datos

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,3 +1,12 @@
 export namespace ConfiguracionSeguridad {
   export const claveJWT: string = "Admin@2023*";
+  export const menuUsuarioId = "64091cab7d4a8e34c4ae53d6";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
+
+
+
 }
