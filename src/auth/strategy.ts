af61c2d9c14c9afb75f608bebcc5bdd276217d14
vanessa <vanessa.1702011634@ucaldas.edu.co>
2023-03-19 16:13:30 -0500

    estrategia basica y como se usa

--- src/auth/strategy.ts
+++ src/auth/strategy.ts
@@ -1,18 +1,16 @@
 import {AuthenticationStrategy} from '@loopback/authentication';
 import {Request} from '@loopback/rest';
 import {UserProfile} from '@loopback/security';
-export class BasicAuthenticationStrategy implements AuthenticationStrategy {
-  name: string = 'basic';
+export class AuthStrategy implements AuthenticationStrategy {
+  name: string = 'auth';
 
   constructor(
 
   ) { }
 
   async authenticate(request: Request): Promise<UserProfile | undefined> {
-
-
+    console.log("ejecutando la estrategia");
     return undefined;
   }
-
 
 }
